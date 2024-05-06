c5f3e4253f0400bc1c2b1d294f4a49b37c67ef81
DaG <christos.daglaroglou@gmail.com>
2024-05-07 01:03:59 +0300

    Update Roles.tsx

--- src/components/discord/Roles.tsx
+++ src/components/discord/Roles.tsx
@@ -5,7 +5,7 @@ export default function Roles() {
       <h2 className="font-bold text-xs text-slate-300 leading-4 mb-2 uppercase">Roles</h2>
       <div className="flex">
 
-        <div className="mb-1 mr-1 p-1 font-medium text-xs flex items-center bg-slate-200 dark:bg-zinc-800 rounded" aria-label="TypeScript" role="listitem"><div className="relative items-center"><span className="w-3 h-3 rounded-full mx-1 shrink-0 flex bg-blue-600"></span></div><div className="max-w-[200px] whitespace-nowrap text-ellipsis mr-1">TypeScript</div></div>
+        <div className="mb-1 mr-1 p-1 font-medium text-xs flex items-center bg-slate-200 dark:bg-zinc-800 rounded" aria-label="TypeScript" role="listitem"><div className="relative items-center"><span className="w-3 h-3 rounded-full mx-1 shrink-0 flex bg-blue-600"></span></div><div className="max-w-[200px] whitespace-nowrap text-ellipsis mr-1">C</div></div>
         <div className="mb-1 mr-1 p-1 font-medium text-xs flex items-center bg-slate-200 dark:bg-zinc-800 rounded" aria-label="JavaScript" role="listitem"><div className="relative items-center"><span className="w-3 h-3 rounded-full mx-1 shrink-0 flex bg-blue-400"></span></div><div className="max-w-[200px] whitespace-nowrap text-ellipsis mr-1">Python</div></div>
         <div className="mb-1 mr-1 p-1 font-medium text-xs flex items-center bg-slate-200 dark:bg-zinc-800 rounded" aria-label="React" role="listitem"><div className="relative items-center"><span className="w-3 h-3 rounded-full mx-1 shrink-0 flex bg-orange-400"></span></div><div className="max-w-[200px] whitespace-nowrap text-ellipsis mr-1">HTML</div></div>
         </div>
