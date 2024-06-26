778ed806e261083cc07c949d3cde7e130137b1dc
DaG <christos.daglaroglou@gmail.com>
2024-06-26 20:08:17 +0300

    Update

--- src/components/discord/Connections.tsx
+++ src/components/discord/Connections.tsx
@@ -39,17 +39,17 @@ export default function Connections() {
         </div>
 
         {/* Valorant */}
-        {/*
-        <div className="h-10 mb-2 text-md w-auto relative flex items-center px-4 justify-between text-white bg-zinc-700 rounded">
+        {
+        <div className="h-12 mb-2 text-md w-auto relative flex items-center px-4 justify-between text-white border border-transparent rounded">
           <div className="flex-row flex text-center items-center whitespace-nowrap text-ellipsis overflow-hidden space-x-1">
             <RiotIcon />
-            <span className="text-md font-semibold text-white">R3FL3X#1337</span>
+            <span className="text-md font-semibold text-white">DaG#1337</span>
             <div className="w-[16px] h-[16px]">
               <VerifiedIcon />
             </div>
           </div>
         </div>
-        /*}
+        }
         
         {/* Twitter */}
         {
--- src/components/discord/Roles.tsx
+++ src/components/discord/Roles.tsx
@@ -10,8 +10,8 @@ export default function Roles() {
         <div className="mb-1 mr-1 p-1 font-medium text-xs flex items-center bg-slate-200 dark:bg-zinc-800 rounded" aria-label="React" role="listitem"><div className="relative items-center"><span className="w-3 h-3 rounded-full mx-1 shrink-0 flex bg-orange-400"></span></div><div className="max-w-[200px] whitespace-nowrap text-ellipsis mr-1">HTML</div></div>
         </div>
         <div className="flex">
-        <div className="mb-1 mr-1 p-1 font-medium text-xs flex items-center bg-slate-200 dark:bg-zinc-800 rounded" aria-label="Elixir" role="listitem"><div className="relative items-center"><span className="w-3 h-3 rounded-full mx-1 shrink-0 flex bg-violet-700"></span></div><div className="max-w-[200px] whitespace-nowrap text-ellipsis mr-1">CSS</div></div>
-        <div className="mb-1 mr-1 p-1 font-medium text-xs flex items-center bg-slate-200 dark:bg-zinc-800 rounded" aria-label="Rust" role="listitem"><div className="relative items-center"><span className="w-3 h-3 rounded-full mx-1 shrink-0 flex bg-green-700"></span></div><div className="max-w-[200px] whitespace-nowrap text-ellipsis mr-1">Batch</div></div>
+        <div className="mb-1 mr-1 p-1 font-medium text-xs flex items-center bg-slate-200 dark:bg-zinc-800 rounded" aria-label="Elixir" role="listitem"><div className="relative items-center"><span className="w-3 h-3 rounded-full mx-1 shrink-0 flex bg-violet-700"></span></div><div className="max-w-[200px] whitespace-nowrap text-ellipsis mr-1">C#</div></div>
+        <div className="mb-1 mr-1 p-1 font-medium text-xs flex items-center bg-slate-200 dark:bg-zinc-800 rounded" aria-label="Rust" role="listitem"><div className="relative items-center"><span className="w-3 h-3 rounded-full mx-1 shrink-0 flex bg-green-700"></span></div><div className="max-w-[200px] whitespace-nowrap text-ellipsis mr-1">Shell</div></div>
     </div>
     </div>
   );
