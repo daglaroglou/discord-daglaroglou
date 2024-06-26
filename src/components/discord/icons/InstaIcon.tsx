5f21b67d7aa61496849d261ed04d7c037a06a774
DaG <christos.daglaroglou@gmail.com>
2024-06-26 20:17:14 +0300

    update

--- src/components/discord/icons/InstaIcon.tsx
+++ src/components/discord/icons/InstaIcon.tsx
@@ -1,4 +1,4 @@
-export default function InstagramIcon() {
+export default function InstaIcon() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
       <path 
